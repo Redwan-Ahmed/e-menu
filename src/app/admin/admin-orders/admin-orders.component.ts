99c17c509e9cd90fe8b5e26b3cb05ec75e883f03
Redwan Ahmed <redwanahmed1@hotmail.co.uk>
2019-04-10 15:20:43 +0100

    Added more comments

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OrderService } from 'src/app/order.service';
-import { Order } from 'src/app/models/order';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -10,6 +9,7 @@ import { Subscription } from 'rxjs';
 })
 export class AdminOrdersComponent implements OnInit, OnDestroy {
 
+/** Here all local variables are declared in side this component.ts file */
   orders$: any;
   orderSubscription: Subscription;
 
@@ -20,17 +20,27 @@ export class AdminOrdersComponent implements OnInit, OnDestroy {
   }
 
   getAllOrders() {
+  /** The orderSubscription variable is subscribing to all orders in the orders collection in firebase.
+   *  This is done by calling the OrderService, which then allows me to use the getAllOrders() function, which I then subscribe to.
+   * the variable orders$ is then set to orders, so that it can be called in the component.html file.
+  */
     this.orderSubscription = this.orderService.getAllOrders().subscribe(orders => {
       this.orders$ = orders;
-      console.log();
     });
   }
 
+  /** The start() function calls the OrderService: startOrder() function, which takes id as a parameter.
+   *  The id is passed thorugh the html file, when the button is clicked (line 36: admin-orders.component.html)
+  */
   start(id){
     this.orderService.startOrder(id);
   }
 
   ngOnDestroy() {
+  /** Here I destroy the subscription or unsubscribe
+   *  This is done to avoid memory leaks, 
+   *  and to allow us to detect changes so it can unsubscribe and then subscribe to new changes.
+   */
     this.orderSubscription.unsubscribe();
   }
 
